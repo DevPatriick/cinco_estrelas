c8871bb95d33a4dc94805bce7b218bacd5991ecd
Patrick Andrade <andrade.patrickreis@gmail.com>
2023-09-11 22:54:11 -0300

    subindo

--- src/pages/home/moviments/faq/faq.js
+++ src/pages/home/moviments/faq/faq.js
@@ -69,3 +69,103 @@ function fechar_four(){
 
 question_4.addEventListener("click", abrir_four);
 response_4.addEventListener("click", fechar_four);
+
+// five
+
+const question_5 = document.querySelector("#question_five");
+const response_5 = document.querySelector("#response_five");
+
+function abrir_five(){
+    if(response_5.style.display="none"){
+        response_5.style.display="block";
+    } 
+}
+
+function fechar_five(){
+    if(response_5.style.display="block"){
+        response_5.style.display="none";
+    } 
+}
+
+question_5.addEventListener("click", abrir_five);
+response_5.addEventListener("click", fechar_five);
+
+// six
+
+
+const question_6 = document.querySelector("#question_six");
+const response_6 = document.querySelector("#response_six");
+
+function abrir_six(){
+    if(response_6.style.display="none"){
+        response_6.style.display="block";
+    } 
+}
+
+function fechar_six(){
+    if(response_6.style.display="block"){
+        response_6.style.display="none";
+    } 
+}
+
+question_6.addEventListener("click", abrir_six);
+response_6.addEventListener("click", fechar_six);
+
+
+const question_7 = document.querySelector("#question_seven");
+const response_7 = document.querySelector("#response_seven");
+
+function abrir_seven(){
+    if(response_7.style.display="none"){
+        response_7.style.display="block";
+    } 
+}
+
+function fechar_seven(){
+    if(response_7.style.display="block"){
+        response_7.style.display="none";
+    } 
+}
+
+question_7.addEventListener("click", abrir_seven);
+response_7.addEventListener("click", fechar_seven);
+
+
+const question_8 = document.querySelector("#question_eight");
+const response_8 = document.querySelector("#response_eight");
+
+function abrir_eight(){
+    if(response_8.style.display="none"){
+        response_8.style.display="block";
+    } 
+}
+
+function fechar_eight(){
+    if(response_8.style.display="block"){
+        response_8.style.display="none";
+    } 
+}
+
+question_8.addEventListener("click", abrir_eight);
+response_8.addEventListener("click", fechar_eight);
+
+
+const question_9 = document.querySelector("#question_nine");
+const response_9 = document.querySelector("#response_nine");
+
+function abrir_nine(){
+    if(response_9.style.display="none"){
+        response_9.style.display="block";
+    } 
+}
+
+function fechar_nine(){
+    if(response_9.style.display="block"){
+        response_9.style.display="none";
+    } 
+}
+
+question_9.addEventListener("click", abrir_nine);
+response_9.addEventListener("click", fechar_nine);
+
+
